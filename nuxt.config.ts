645eec4224c370557ba4cbf4e60b08238cad1cb0
Soulinmaikadua <Soulinmaikadua@gmail.com>
2022-12-15 21:19:53 +0700

    new commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     ssr:false,
+    
+    nitro: {
+      preset: 'node-server'
+    },
+    target: 'static',
     app: {
     head: {
       charset: 'utf-16',
@@ -12,6 +17,7 @@ export default defineNuxtConfig({
       ],
     }
     },
+    
     css: ['vuetify/lib/styles/main.sass', '@mdi/font/css/materialdesignicons.min.css'],
     build: {
         transpile: ['vuetify'],
